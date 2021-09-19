0101c4b40ac05d522d265d05f37ef1f531e764bc
yuanrong Chen <yuanrong.ch@foxmail.com>
2021-09-19 21:45:20 +0800

    Rearrange exercises code

--- main.cpp
+++ main.cpp
@@ -5,17 +5,27 @@
 #include <iostream>
 
 
-int main(int, char**) {
-
+void Exercises4_1() {
     cyr::Ray ray = {Eigen::Vector3f(1, 1, 1), Eigen::Vector3f(-1, -1, -1),};
     cyr::Sphere sphere({0, 0, 0}, 1);
+    cyr::HitRecord hitRecord;
+    printf("Answer to 4-1: %d\n", sphere.Hit(ray, 0, 50000, &hitRecord));
+}
+
+void Exercises4_2() {
+    cyr::Ray ray = {Eigen::Vector3f(1, 1, 1), Eigen::Vector3f(-1, -1, -1),};
     float ver[3][3] = {
         {1, 0, 0},
         {0, 1, 0},
         {0, 0, 1}
     };
     cyr::Triangle tri(ver);
-
     cyr::HitRecord hitRecord;
-    printf("%d\n", tri.Hit(ray, 0, 50000, &hitRecord));
+    printf("Answer to 4-2: %d\n", tri.Hit(ray, 0, 50000, &hitRecord));
+}
+
+int main(int, char**) {
+    Exercises4_1();
+    Exercises4_2();
+    
 }
